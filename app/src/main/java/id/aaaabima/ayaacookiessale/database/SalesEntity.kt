af8c791d131ca9f682cfee23c57dde753da9f0ea
aaaabima <aaaabim@gmail.com>
2024-07-27 17:24:13 +0700

    feat: adds SalesEntity class

---
+++ app/src/main/java/id/aaaabima/ayaacookiessale/database/SalesEntity.kt
@@ -0,0 +1,14 @@
+package id.aaaabima.ayaacookiessale.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "sales_table")
+data class SalesEntity(
+  @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
+  @ColumnInfo(name = "cake") val cake: String,
+  @ColumnInfo(name = "price") val price: String,
+  @ColumnInfo(name = "description") val description: String,
+  @ColumnInfo(name = "quantity") val quantity: String
+)
