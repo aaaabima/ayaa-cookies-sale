73e4ee51a8a55f01d3b314662b67934abda31824
aaaabima <aaaabim@gmail.com>
2024-07-27 17:24:56 +0700

    feat: adds SalesDao with necessary SQLite queries

---
+++ app/src/main/java/id/aaaabima/ayaacookiessale/database/SalesDao.kt
@@ -0,0 +1,34 @@
+package id.aaaabima.ayaacookiessale.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SalesDao {
+
+  @Query("SELECT * FROM sales_table ORDER BY id DESC")
+  fun getSales(): Flow<List<SalesEntity>>
+
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
+  suspend fun addSales(sales: SalesEntity)
+
+  @Query("DELETE FROM sales_table WHERE id = :id")
+  suspend fun deleteSalesById(id: Int)
+
+  @Query("DELETE FROM sales_table")
+  suspend fun deleteAllSales()
+
+  @Query("UPDATE sales_table SET cake = :cake, price = :price, description = :description, quantity = :quantity WHERE id = :id")
+  suspend fun updateSales(id: Int, cake: String, price: String, description: String, quantity: String)
+
+  @Delete
+  suspend fun deleteSales(sales: SalesEntity)
+
+  @Update
+  suspend fun updateSales(sales: SalesEntity)
+}
