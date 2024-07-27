63377551fd99b5757c9d4bcfdfef15bed657c7e2
aaaabima <aaaabim@gmail.com>
2024-07-27 17:25:33 +0700

    feat: create repository to create access between database and UI

---
+++ app/src/main/java/id/aaaabima/ayaacookiessale/database/SalesRepository.kt
@@ -0,0 +1,39 @@
+package id.aaaabima.ayaacookiessale.database
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+class SalesRepository(private val salesDao: SalesDao) {
+
+  val allSales: Flow<List<SalesEntity>> = salesDao.getSales()
+
+  @WorkerThread
+  suspend fun addSales(sales: SalesEntity) {
+    salesDao.addSales(sales)
+  }
+
+  @WorkerThread
+  suspend fun deleteSalesById(id: Int) {
+    salesDao.deleteSalesById(id)
+  }
+
+  @WorkerThread
+  suspend fun deleteAllSales() {
+    salesDao.deleteAllSales()
+  }
+
+  @WorkerThread
+  suspend fun updateSales(id: Int, cake: String, price: String, description: String, quantity: String) {
+    salesDao.updateSales(id, cake, price, description, quantity)
+  }
+
+  @WorkerThread
+  suspend fun deleteSales(sales: SalesEntity) {
+    salesDao.deleteSales(sales)
+  }
+
+  @WorkerThread
+  suspend fun updateSales(sales: SalesEntity) {
+    salesDao.updateSales(sales)
+  }
+}
