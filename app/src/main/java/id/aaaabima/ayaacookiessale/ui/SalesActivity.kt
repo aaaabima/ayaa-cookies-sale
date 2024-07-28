6a182c6ab8c4243827d4a7d8f45dd69e9e17b33c
aaaabima <aaaabim@gmail.com>
2024-07-28 14:15:16 +0700

    feat: implement CRUD on SalesActivity

--- app/src/main/java/id/aaaabima/ayaacookiessale/ui/SalesActivity.kt
+++ app/src/main/java/id/aaaabima/ayaacookiessale/ui/SalesActivity.kt
@@ -1,19 +1,33 @@
 package id.aaaabima.ayaacookiessale.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import id.aaaabima.ayaacookiessale.SalesApplication
 import id.aaaabima.ayaacookiessale.databinding.ActivitySalesBinding
 import id.aaaabima.ayaacookiessale.ui.adapter.SalesListAdapter
 import id.aaaabima.ayaacookiessale.ui.model.SalesModel
+import id.aaaabima.ayaacookiessale.ui.model.toListSalesModel
+import id.aaaabima.ayaacookiessale.ui.model.toSalesEntity
+import id.aaaabima.ayaacookiessale.ui.viewmodel.SalesViewModel
+import id.aaaabima.ayaacookiessale.ui.viewmodel.SalesViewModelFactory
 
 class SalesActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySalesBinding
     private var salesAdapter: SalesListAdapter? = null
+    private val salesViewModel: SalesViewModel by viewModels {
+        SalesViewModelFactory((application as SalesApplication).salesRepository)
+    }
+
+    private lateinit var getResult: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +40,16 @@ class SalesActivity : AppCompatActivity() {
             insets
         }
 
+        initGetActivityResult()
         initAdapter()
+        initObserver()
+        initListener()
+    }
+
+    private fun initObserver() {
+        salesViewModel.allSales.observe(this) { sales ->
+            sales?.let { salesAdapter?.submitList(it.toListSalesModel()) }
+        }
     }
 
     private fun initAdapter() {
@@ -37,7 +60,47 @@ class SalesActivity : AppCompatActivity() {
         }
     }
 
+    private fun initListener() {
+        binding.fabAddSales.setOnClickListener {
+            val intent = Intent(this@SalesActivity, AddNewSalesActivity::class.java)
+            getResult.launch(intent)
+        }
+    }
+
+    private fun initGetActivityResult() {
+        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> result.data?.getParcelableExtra<SalesModel>(AddNewSalesActivity.EXTRA_REPLY)?.let {
+                    salesViewModel.addSales(it.toSalesEntity())
+                }
+
+                RESULT_CODE_DELETE -> result.data?.getParcelableExtra<SalesModel>(EXTRA_SALES_DATA)?.let {
+                    salesViewModel.deleteSales(it.toSalesEntity())
+                }
+
+                RESULT_CODE_UPDATE -> result.data?.getParcelableExtra<SalesModel>(EXTRA_SALES_DATA)?.let {
+                    salesViewModel.updateSales(it.toSalesEntity())
+                }
+
+                RESULT_CODE_FAIL_INSERT -> Toast.makeText(
+                    applicationContext,
+                    "Kue tidak ditambahkan karena data tidak lengkap.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
     private val onItemClicked : (SalesModel) -> Unit = { salesModel: SalesModel ->
-        Toast.makeText(this, salesModel.cake, Toast.LENGTH_SHORT).show()
+        val intent = Intent(this@SalesActivity, SalesDetailActivity::class.java)
+        intent.putExtra(EXTRA_SALES_DATA, salesModel)
+        getResult.launch(intent)
+    }
+
+    companion object {
+        const val EXTRA_SALES_DATA = "SALES_DATA"
+        const val RESULT_CODE_FAIL_INSERT = 2
+        const val RESULT_CODE_DELETE = 3
+        const val RESULT_CODE_UPDATE = 4
     }
 }
